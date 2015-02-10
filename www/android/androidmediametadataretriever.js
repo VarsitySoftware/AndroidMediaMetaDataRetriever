f4df91b4e42f407d6ffec387d77296b56ef3d121
VarsitySoftware <github@varsitysoftware.com>
2015-02-10 11:52:58 -0500

    Update androidmediametadataretriever.js

--- www/android/androidmediametadataretriever.js
+++ www/android/androidmediametadataretriever.js
@@ -1,23 +1,7 @@
-//cordova.define("com.varsitysoftware.cordova.androidmediametadataretriever.AndroidMediaMetaDataRetriever", function (require, exports, module) {
-
-//    window.getAndroidMetadata = function (strFilePath, strFileType, onStart, onFail) { exec(onStart, onFail, "AndroidMediaMetaDataRetriever", "getAndroidMetadata", [strFilePath, strFileType]); };
-
-//});
-
 cordova.define("com.varsitysoftware.cordova.androidmediametadataretriever.AndroidMediaMetaDataRetriever", function (require, exports, module) {
 
-    var exec = require("cordova/exec");
-    var AndroidMediaMetaDataRetriever = function () { };
+    window.getAndroidMetadata = function (strFilePath, strFileType, onStart, onFail) { exec(onStart, onFail, "AndroidMediaMetaDataRetriever", "getAndroidMetadata", [strFilePath, strFileType]); };
 
-    AndroidMediaMetaDataRetriever.prototype.getAndroidMetadata = function (strFilePath, strFileType, onStart, onFail) {
-        alert(AndroidMediaMetaDataRetriever);
-        exec(onStart, onFail, "AndroidMediaMetaDataRetriever", "getAndroidMetadata", [strFilePath, strFileType]);
-    };
-
-    var androidMediaMetaDataRetriever = new AndroidMediaMetaDataRetriever();
-    module.exports = androidMediaMetaDataRetriever;
 });
 
-if (!window.plugins) {
-    window.plugins = {};
-}
+
